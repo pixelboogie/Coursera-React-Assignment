589127618353818f083247a09459d4f6c07c7b5b
Shane Anderson <shane@pixelboogie.com>
2024-06-10 10:21:39 -0500

    Added ability to display JSX and React messages

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,17 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// JSX expression
+const greeting = <h1>"Welcome, please enjoy your visit."</h1>
+
+// Using React to create element
+const msg1 = React.createElement('h2', null, 'It\'s a very simple app.')
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    {greeting} {/* displatying the jsx expression */}
+    {msg1} {/* displatying the React expression */}
+
   </React.StrictMode>,
 )
